b4766fe41a99c87554c7edaae20f35521b3b32ad
xeaarly <akia.chunn@gmail.com>
2026-05-31 05:54:50 +0800

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -20,8 +20,8 @@ module.exports = {
         const balanceEmbed = new EmbedBuilder()
             .setColor(0xffb7c5)
             .setAuthor({ name: `${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() })
-            .setDescription(`⌗﹒ <a:05_Clover:1505555784266940640> ﹒**${target.username}'s Palette** ﹒︵  \n\n<a:dcr_a1:1501886035402031115> ﹙✦﹚  **\`${user.wallet}\` palette pieces!** ﹒ <a:tumblr_91a792399801929b62e23ca8f:1508609247246549094>  \n\n<a:dcr_h:1505324134861770883> ﹒**Keep creating!**  ◟  >`)
-            .setFooter({ text: `every brush stroke counts` });
+            .setDescription(`⌗﹒ <a:05_Clover:1505555784266940640> ﹒**${target.username}'s Palette** ﹒︵  \n\n<a:dcr_a1:1501886035402031115> ﹙✦﹚  **\`${user.wallet}\` palette pieces!** ﹒ <a:tumblr_91a792399801929b62e23ca8f:1508609247246549094> ◟  `)
+            .setFooter({ text: `キ﹒every brush stroke counts` });
         
         await interaction.reply({ embeds: [balanceEmbed] });
     }
