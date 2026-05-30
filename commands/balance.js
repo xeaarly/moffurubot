0c6682b80b7fb96c4cab5a87a2ca48dffb6d7705
xeaarly <akia.chunn@gmail.com>
2026-05-31 05:52:34 +0800

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 const userData = new Map();
 
@@ -17,6 +17,12 @@ module.exports = {
         
         const user = userData.get(userId) || { wallet: 0 };
         
-        await interaction.reply(`🎨 **${target.username}** has **${user.wallet}** palette pieces!`);
+        const balanceEmbed = new EmbedBuilder()
+            .setColor(0xffb7c5)
+            .setAuthor({ name: `${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() })
+            .setDescription(`⌗﹒ <a:05_Clover:1505555784266940640> ﹒**${target.username}'s Palette** ﹒︵  \n\n<a:dcr_a1:1501886035402031115> ﹙✦﹚  **\`${user.wallet}\` palette pieces!** ﹒ <a:tumblr_91a792399801929b62e23ca8f:1508609247246549094>  \n\n<a:dcr_h:1505324134861770883> ﹒**Keep creating!**  ◟  >`)
+            .setFooter({ text: `every brush stroke counts` });
+        
+        await interaction.reply({ embeds: [balanceEmbed] });
     }
 };
