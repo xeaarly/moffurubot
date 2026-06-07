f9499f3b0c4abf801772509752d7eeae982f0ad1
xeaarly <akia.chunn@gmail.com>
2026-06-08 00:44:29 +0800

    Fixed placeholder replacement in autoresponder embeds

--- handlers/autoresponderHandler.js
+++ handlers/autoresponderHandler.js
@@ -6,7 +6,7 @@ async function handleAutoresponder(message) {
     const content = message.content.toLowerCase();
     
     for (const [trigger, data] of Object.entries(triggers)) {
-        if (content.includes(trigger)) {
+        if (content.includes(trigger.toLowerCase())) {
             const embedMatch = data.response.match(/{embed:([^}]+)}/);
             
             if (embedMatch) {
@@ -15,18 +15,94 @@ async function handleAutoresponder(message) {
                 const embedData = savedEmbeds[embedName];
                 
                 if (embedData) {
+                    const user = message.author;
                     const embed = new EmbedBuilder().setColor(embedData.color || 0xffb7c5);
-                    if (embedData.authorName) embed.setAuthor({ name: embedData.authorName.replace(/{user}/g, message.author.username) });
-                    if (embedData.title) embed.setTitle(embedData.title.replace(/{user}/g, message.author.username));
-                    if (embedData.description) embed.setDescription(embedData.description.replace(/{user}/g, message.author.username).replace(/{newline}/g, '\n'));
-                    if (embedData.thumbnail) embed.setThumbnail(embedData.thumbnail);
-                    if (embedData.image) embed.setImage(embedData.image);
-                    if (embedData.footerText) embed.setFooter({ text: embedData.footerText.replace(/{user}/g, message.author.username) });
+                    
+                    // Author - handle placeholders
+                    if (embedData.authorName) {
+                        let authorText = embedData.authorName
+                            .replace(/{user}/g, user.username)
+                            .replace(/{user_name}/g, user.username)
+                            .replace(/{user_tag}/g, user.username);
+                        let authorIcon = null;
+                        if (embedData.authorIcon) {
+                            if (embedData.authorIcon === '{user_avatar}' || embedData.authorIcon === '{user_icon}') {
+                                authorIcon = user.displayAvatarURL();
+                            } else {
+                                authorIcon = embedData.authorIcon;
+                            }
+                        }
+                        embed.setAuthor({ name: authorText, iconURL: authorIcon });
+                    }
+                    
+                    // Title
+                    if (embedData.title) {
+                        embed.setTitle(embedData.title
+                            .replace(/{user}/g, user.username)
+                            .replace(/{user_name}/g, user.username)
+                            .replace(/{user_tag}/g, user.username));
+                    }
+                    
+                    // Description
+                    if (embedData.description) {
+                        embed.setDescription(embedData.description
+                            .replace(/{user}/g, user.username)
+                            .replace(/{user_name}/g, user.username)
+                            .replace(/{user_tag}/g, user.username)
+                            .replace(/{newline}/g, '\n'));
+                    }
+                    
+                    // Thumbnail
+                    if (embedData.thumbnail) {
+                        let thumbnail = embedData.thumbnail;
+                        if (thumbnail === '{user_avatar}' || thumbnail === '{user_icon}') {
+                            thumbnail = user.displayAvatarURL();
+                        }
+                        embed.setThumbnail(thumbnail);
+                    }
+                    
+                    // Image
+                    if (embedData.image) {
+                        let image = embedData.image;
+                        if (image === '{user_avatar}' || image === '{user_icon}') {
+                            image = user.displayAvatarURL();
+                        }
+                        embed.setImage(image);
+                    }
+                    
+                    // Footer
+                    if (embedData.footerText) {
+                        let footerText = embedData.footerText
+                            .replace(/{user}/g, user.username)
+                            .replace(/{user_name}/g, user.username)
+                            .replace(/{user_tag}/g, user.username);
+                        let footerIcon = null;
+                        if (embedData.footerIcon) {
+                            if (embedData.footerIcon === '{user_avatar}' || embedData.footerIcon === '{user_icon}') {
+                                footerIcon = user.displayAvatarURL();
+                            } else {
+                                footerIcon = embedData.footerIcon;
+                            }
+                        }
+                        embed.setFooter({ text: footerText, iconURL: footerIcon });
+                    }
+                    
+                    // Timestamp
+                    if (embedData.timestamp) {
+                        embed.setTimestamp();
+                    }
                     
                     await message.reply({ embeds: [embed] });
+                } else {
+                    await message.reply({ content: `❌ Embed "${embedName}" not found!` });
                 }
             } else {
-                let response = data.response.replace(/{user}/g, message.author.username).replace(/{newline}/g, '\n');
+                // Text response with placeholders
+                let response = data.response
+                    .replace(/{user}/g, message.author.username)
+                    .replace(/{user_name}/g, message.author.username)
+                    .replace(/{user_tag}/g, message.author.username)
+                    .replace(/{newline}/g, '\n');
                 await message.reply({ content: response });
             }
             break;
